ba00db3a7a17049b0519e7de19abbfec5ca872ad
Vladislav <vlad.murashchenko@gmail.com>
2020-11-03 22:27:46 +0200

    basic App, Header, Footer markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
+
 import s from './App.module.scss';
 
 const App: React.FC = () => {
-  return <h1 className={s.heading}>Hi there!</h1>;
+  return (
+    <div className={s.wrapper}>
+      <AppHeader />
+      <main className={s.main}>some content</main>
+      <AppFooter />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AppFooter = () => {
+  return (
+    <footer>
+      <div>
+        <span>Make with</span>
+        <img alt="love" />
+      </div>
+      <div>Our Teams</div>
+    </footer>
+  );
+};
+
+export default AppFooter;
