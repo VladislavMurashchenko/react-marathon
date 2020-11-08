db1e276d6ec9e7a081b6db124d579905feda04d0
Vladislav <vlad.murashchenko@gmail.com>
2020-11-09 00:56:05 +0200

    add Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AppFooter from './components/AppFooter';
 import Layout from './components/Layout';
 import Parallax from './components/Parallax';
 import Button from './components/Button';
+import Heading from './components/Heading';
 
 import s from './App.module.scss';
 
@@ -15,14 +16,14 @@ const App: React.FC = () => {
       <main className={s.main}>
         <Layout className={s.layout}>
           <div className={s.textWrapper}>
-            <h1 className={s.title}>
+            <Heading as="h1">
               <span className={s.bold}>Find</span> all your favorite{' '}
               <span className={s.bold}>Pokemon</span>
-            </h1>
-            <p className={s.note}>
+            </Heading>
+            <Heading as="h2">
               You can know the type of Pokemon, its strengths, disadvantages and
               abilities
-            </p>
+            </Heading>
             <Button onClick={() => {}}>See pokemons</Button>
           </div>
           <Parallax />
