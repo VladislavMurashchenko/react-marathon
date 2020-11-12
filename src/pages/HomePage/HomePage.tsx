2b61a288995f9764d5eb1948b5a92d25ea63660a
Vladislav <vlad.murashchenko@gmail.com>
2020-11-12 21:22:26 +0200

    add LinksEnum

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { navigate } from 'hookrouter';
 
+import s from './HomePage.module.scss';
+import { LinkEnum } from '../../routes';
+
 import Layout from '../../components/Layout';
 import Parallax from '../../components/Parallax';
 import Button from '../../components/Button';
@@ -8,8 +11,6 @@ import Heading from '../../components/Heading';
 
 import AppHeader from '../../components/AppHeader';
 import AppFooter from '../../components/AppFooter';
-
-import s from './HomePage.module.scss';
 
 const HomePage: React.FC = () => {
   return (
@@ -26,7 +27,9 @@ const HomePage: React.FC = () => {
               You can know the type of Pokemon, its strengths, disadvantages and
               abilities
             </Heading>
-            <Button onClick={() => navigate('/pokedex')}>See pokemons</Button>
+            <Button onClick={() => navigate(LinkEnum.POKEDEX)}>
+              See pokemons
+            </Button>
           </div>
           <Parallax />
         </Layout>
